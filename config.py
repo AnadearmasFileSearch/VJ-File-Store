2e9e980886d19ed09a0b594fca98522de77f9686
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-12 16:09:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = environ.get("API_HASH", "c949caf0c7d5c268bd8302a8a730027b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7681476895:AAHogca8nltIOrJSg0E3nfxrLXDy5-3v5UE")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/94ba74f622ee2b4f21ad6.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1490611763').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Queen_Jenna_Ortega_bot") # without @
 PORT = environ.get("PORT", "8080")
 
